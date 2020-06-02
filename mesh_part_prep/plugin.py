5cf03eaf85bec11cd15412fdabdeb08e5067a8b5
Paul Gierz <pgierz@awi.de>
2020-06-02 10:17:29 +0200

    working plugin functionality

--- mesh_part_prep/plugin.py
+++ mesh_part_prep/plugin.py
@@ -2,6 +2,9 @@ import mesh_part_prep
 
 
 def esm_mesh_part_prep(config):
+    """
+    Runs a pre-process script for the mesh partioner to generate 3D information
+    """
     if config.get("general", {}).get("mesh_part_prep", False):
         mesh_dir = config["fesom"]["mesh_dir"]
         rm = mesh_part_prep.RawMesh(path=mesh_dir)
